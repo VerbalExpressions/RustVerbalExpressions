e7217435da115c452444989bb97383cf8bf67917
Alexander Popiak <alexander.popiak@gmail.com>
2016-01-08 17:28:46 +0100

    add second example

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,45 @@
 //!
 //! # Examples
 //!
-//! Here's a simple example shamelessly stolen from the python VerEx readme:
+//! ```rust
+//! # extern crate verex; use verex::VerEx;
+//!
+//! # fn main() {
+//!     // You can either use a mutable verx to define different regexes
+//!     let mut verex = VerEx::new();
+//!     let regex1 = verex.find("a")
+//!                       .compile()
+//!                       .unwrap();
+//!
+//!     let regex2 = verex.or_find("b")
+//!                       .compile()
+//!                       .unwrap();
+//!
+//!     // Or just use it for building one
+//!     let regex3 = VerEx::new().find("a")
+//!                              .or_find("b")
+//!                              .compile()
+//!                              .unwrap();
+//!
+//!     // Test if the URL is valid
+//!     assert!(!regex1.is_match("b"));
+//!     assert!(regex2.is_match("b"));
+//!     assert!(regex3.is_match("b"));
+//!
+//!     // Test the generated regex string
+//!     assert_eq!(regex1.as_str(), r"(?:a)");
+//!     assert_eq!(regex2.as_str(), r"(?:a)|(?:b)");
+//!     assert_eq!(regex3.as_str(), r"(?:a)|(?:b)");
+//! # }
+//! ```
+//!
+//!
+//! Here's a URL testing example shamelessly stolen from the python VerEx readme:
 //!
 //! ```rust
-//! extern crate verex;
+//! # extern crate verex; use verex::VerEx;
 //!
-//! use verex::VerEx;
-//!
-//! fn main() {
+//! # fn main() {
 //!     // Create an example of how to test for correctly formed URLs
 //!     let mut verex = VerEx::new();
 //!     let regex = verex
@@ -32,9 +63,9 @@
 //!     // Test if the URL is valid
 //!     assert!(regex.is_match(test_url));
 //!
-//!     // Print the generated regex
-//!     assert_eq!(verex.source(), r"^(?:http)(?:s)?(?:://)(?:www.)?(?:[^ ]*)$")
-//! }
+//!     // Test the generated regex string
+//!     assert_eq!(verex.source(), r"^(?:http)(?:s)?(?:://)(?:www.)?(?:[^ ]*)$");
+//! # }
 //! ```
 
 #![warn(missing_docs)]
